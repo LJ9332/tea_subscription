db9c9e7537d8e9bd4b31223b0219380f030b21c5
lance butler <lancej2004@gmail.com>
2024-08-20 01:49:06 -0600

    Test: creates test for cancel endpoint

--- spec/requests/api/v1/customer_subscription_request_spec.rb
+++ spec/requests/api/v1/customer_subscription_request_spec.rb
@@ -58,8 +58,8 @@ describe "Customer Subscription API" do
       subscription1 = Subscription.create!(title: "10 for 2", price: 10, frequency: 2)
       subscription2 = Subscription.create!(title: "15 for 5", price: 15, frequency: 1)
 
-      customer_subscription1 = CustomerSubscription.create(customer_id: customer.id, subscription_id: subscription1.id, status: "active")
-      customer_subscription2 = CustomerSubscription.create(customer_id: customer.id, subscription_id: subscription2.id, status: "cancelled")
+      customer_subscription1 = CustomerSubscription.create!(customer_id: customer.id, subscription_id: subscription1.id, status: "active")
+      customer_subscription2 = CustomerSubscription.create!(customer_id: customer.id, subscription_id: subscription2.id, status: "cancelled")
       customer_subscriptions = customer.customer_subscriptions
 
       get "/api/v1/customers/#{customer.id}/customer_subscriptions"
@@ -107,6 +107,58 @@ describe "Customer Subscription API" do
       expect(data[0][:relationships][:subscription][:data][:type]).to eq("subscription")
 
       expect(customer_subscriptions.count).to eq(2)
+    end
+
+    it "Can update a customer subscription's status" do
+      customer = Customer.create!(first_name: "LJ", last_name: "Butler", email: "test@test.com", address: "123 test st")
+      subscription = Subscription.create!(title: "10 for 2", price: 10, frequency: 2)
+      customer_sub = CustomerSubscription.create!(customer_id: customer.id, subscription_id: subscription.id)
+
+      expect(customer_sub.status).to eq("active")
+
+      params = { status: "cancelled" }
+      headers = {"CONTENT_TYPE": "application/json"}
+      patch "/api/v1/customer_subscriptions/#{customer_sub.id}/cancel", headers: headers, params: params, as: :json
+      
+      customer_subscription = customer.customer_subscriptions.last
+
+      expect(customer_subscription.status).to eq("cancelled")
+
+      expect(response).to be_successful
+      data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(data).to be_a Hash
+      expect(data).to have_key(:id)
+      expect(data[:id].to_i).to eq(customer_subscription.id)
+      expect(data).to have_key(:type)
+      expect(data[:type]).to eq("customer_subscription")
+      
+      
+      expect(data).to have_key(:attributes)
+      expect(data[:attributes]).to be_a Hash
+      expect(data[:attributes]).to have_key(:status)
+      expect(data[:attributes][:status]).to eq("cancelled")
+      
+      expect(data).to have_key(:relationships)
+      expect(data[:relationships]).to be_a Hash
+
+      expect(data[:relationships]).to have_key(:customer)
+      expect(data[:relationships][:customer]).to be_a Hash
+      expect(data[:relationships][:customer]).to have_key(:data)
+      expect(data[:relationships][:customer][:data]).to be_a Hash
+      expect(data[:relationships][:customer][:data]).to have_key(:id)
+      expect(data[:relationships][:customer][:data][:id].to_i).to eq(customer.id)
+      expect(data[:relationships][:customer][:data]).to have_key(:type)
+      expect(data[:relationships][:customer][:data][:type]).to eq("customer")
+      
+      expect(data[:relationships]).to have_key(:subscription)
+      expect(data[:relationships][:subscription]).to be_a Hash
+      expect(data[:relationships][:subscription]).to have_key(:data)
+      expect(data[:relationships][:subscription][:data]).to be_a Hash
+      expect(data[:relationships][:subscription][:data]).to have_key(:id)
+      expect(data[:relationships][:subscription][:data][:id].to_i).to eq(subscription.id)
+      expect(data[:relationships][:subscription][:data]).to have_key(:type)
+      expect(data[:relationships][:subscription][:data][:type]).to eq("subscription")
     end
   end
 end
