decfad22ad70b2507be71c24f23ab07bed936f1f
lance butler <lancej2004@gmail.com>
2024-08-20 08:33:23 -0600

    Feat: creates update controller action to cancel subscription

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -9,6 +9,13 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
     render json: CustomerSubscriptionSerializer.new(customer_subscription), status: :created
   end
 
+  def update
+    customer_subscription = CustomerSubscription.find(params[:id])
+    if customer_subscription.update!(status: :active)
+      render json: CustomerSubscriptionSerializer.new(customer_subscription)
+    end
+  end
+
   private
   def customer_subscription_params
     params.require(:customer_subscription).permit(:customer_id, :subscription_id)
